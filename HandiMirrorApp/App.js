37ae5d40c544dea77512904e5e402400ee04e975
Tarek Taguine <t.taguine@it-students.fr>
2023-03-28 16:24:22 +0200

    feature : home page
    Home page has been created

--- HandiMirrorApp/App.js
+++ HandiMirrorApp/App.js
@@ -8,12 +8,7 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
-const Colors = {
-  darkGreen: "#5F7161",
-  green: "#6D8B74",
-  beige: "#EFEAD8",
-  grey: "#D0C9C0",
-};
+import { Colors } from "./src/utilities/Colors"
 
 export default function App() {
   return (
