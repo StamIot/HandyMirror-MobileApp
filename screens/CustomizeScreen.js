7f3869e8ecd6dc197fee8ef9f0e7593a3dcb4160
Alain Guillon <a.guillon@it-students.fr>
2023-05-21 20:32:00 +0200

    feature: update content
    add a content for CustomizeScreen

--- screens/CustomizeScreen.js
+++ screens/CustomizeScreen.js
@@ -1,29 +1,122 @@
 // Dépendances
-import { StyleSheet, View, Text, Pressable, Image } from 'react-native'
+import {
+    StyleSheet,
+    View,
+    Text,
+    Pressable,
+    Image,
+    FlatList,
+    Switch,
+} from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 // Styles
 import { Color, FontFamily, FontSize } from '../GlobalStyles'
 
-// Composants Créer
-import TextInputExample from '../Component/TextInput'
+// Data
+const INITIAL_DATA = [
+    {
+        id: 1,
+        name: 'Todo List',
+        describe: 'Les tâches courantes',
+        show: true,
+    },
+    {
+        id: 2,
+        name: 'List item',
+        describe: 'supporting Text',
+        show: false,
+    },
+    {
+        id: 3,
+        name: 'List item',
+        describe: 'supporting Text',
+        show: true,
+    },
+    {
+        id: 4,
+        name: 'List item',
+        describe: 'supporting Text',
+        show: false,
+    },
+    {
+        id: 5,
+        name: 'List item',
+        describe: 'supporting Text',
+        show: false,
+    },
+    {
+        id: 6,
+        name: 'List item',
+        describe: 'supporting Text',
+        show: true,
+    },
+]
 
 const CustomizeScreen = () => {
     const navigation = useNavigation()
 
+    const [data, setData] = useState(INITIAL_DATA)
+    const toggleSwitch = (id) => {
+        setData(
+            data.map((item) =>
+                item.id === id ? { ...item, show: !item.show } : item,
+            ),
+        )
+    }
+
     return (
         <>
-            {/* SCREEN N°3 */}
-            <View style={styles.screen3}>
+            {/* SCREEN N°4 */}
+            <View style={styles.screen4}>
                 {/* CERCLES */}
                 <View style={styles.circlesContainer}>
                     <View style={[styles.circle, styles.circleOne]}></View>
                     <View style={[styles.circle, styles.circleTwo]}></View>
                 </View>
 
-                {/* MESSAGE : Ravi de vous revoir */}
+                {/* Accueil */}
                 <View style={styles.messageContainer}>
-                    <Text style={styles.message}>{`Hi Sophie, `}</Text>
+                    <Image
+                        style={styles.avatar}
+                        source={require('../assets/Default_UserProfilePicture1.png')}
+                    />
+                    <Text style={styles.message}>{`Hi Pseudo, `}</Text>
+                </View>
+
+                {/* Un message */}
+                <View style={styles.textWhiteContainer}>
+                    <Text style={styles.messageTextWhite}>
+                        Que souhaitez vous laissez apparaître sur votre
+                        HandyMirror ?
+                    </Text>
+                </View>
+
+                {/* Liste des éléments de la liste */}
+                <View style={styles.list}>
+                    <FlatList
+                        data={data}
+                        renderItem={({ item }) => (
+                            <View style={styles.listItem}>
+                                <View>
+                                    <Text style={styles.listItemName}>
+                                        {item.name}
+                                    </Text>
+                                    <Text>{item.describe}</Text>
+                                </View>
+                                <View>
+                                    <Switch
+                                        value={item.show}
+                                        onValueChange={() =>
+                                            toggleSwitch(item.id)
+                                        }
+                                    />
+                                </View>
+                            </View>
+                        )}
+                        keyExtractor={(item) => item.id.toString()}
+                    />
                 </View>
 
                 {/* Pas de compte s'enregistrer */}
@@ -45,7 +138,7 @@ const CustomizeScreen = () => {
 }
 
 const styles = StyleSheet.create({
-    screen3: {
+    screen4: {
         flex: 1,
         backgroundColor: Color.dimgray,
     },
@@ -71,6 +164,14 @@ const styles = StyleSheet.create({
     },
     messageContainer: {
         flex: 0.1,
+        flexDirection: 'row',
+    },
+    avatar: {
+        width: 80,
+        height: 80,
+        borderRadius: 50,
+        marginRight: 20,
+        marginLeft: 20,
     },
     message: {
         textAlign: 'center',
@@ -78,6 +179,34 @@ const styles = StyleSheet.create({
         fontSize: FontSize.size_21xl,
         color: Color.antiquewhite,
         paddingTop: 20,
+    },
+    textWhiteContainer: {
+        flex: 0.1,
+    },
+    messageTextWhite: {
+        color: Color.antiquewhite,
+        textAlign: 'center',
+        marginTop: 20,
+        fontSize: 20,
+    },
+    list: {
+        flex: 0.6,
+        padding: 15,
+    },
+    listItem: {
+        backgroundColor: Color.antiquewhite,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        padding: 15,
+        marginTop: 15,
+        borderRadius: 5,
+    },
+    listItemName: {
+        fontWeight: '700',
+        fontSize: 16,
+        color: Color.dimgray,
     },
     formContainer: {
         flex: 0.6,
@@ -124,9 +253,6 @@ const styles = StyleSheet.create({
     },
     textDarkContainer: {
         paddingRight: 15,
-    },
-    textWhiteContainer: {
-        paddingLeft: 15,
     },
 })
 
